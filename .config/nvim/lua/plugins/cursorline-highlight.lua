79e05f16ed25be5c44a50332527e0985b3cdbcbb
Soma Noir <45913147+somanoir@users.noreply.github.com>
2025-11-06 17:31:33 +0300

    nvim: add cursorline plugin

---
+++ .config/nvim/lua/plugins/cursorline-highlight.lua
@@ -0,0 +1,18 @@
+return {
+  "ya2s/nvim-cursorline",
+  enabled = true,
+  config = function()
+    require('nvim-cursorline').setup {
+      cursorline = {
+        enable = true,
+        timeout = 1000,
+        number = false,
+      },
+      cursorword = {
+        enable = true,
+        min_length = 3,
+        hl = { underline = true },
+      }
+    }
+  end,
+}
