54ebee9ddbcc616620c15317cc1eeff418c0f67d
Soma Noir <45913147+somanoir@users.noreply.github.com>
2025-11-08 14:27:48 +0300

    waybar: delete obsolete files and standardise hypr and niri versions

--- .config/waybar/waybar-hypr.sh
+++ .config/waybar/waybar-hypr.sh
@@ -7,4 +7,4 @@ killall -q waybar
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
 # Launch main
-waybar -c ~/.config/waybar/config-hypr.jsonc -s ~/.config/waybar/style-hypr.css &> /dev/null & disown
+waybar -c ~/.config/waybar/config-hypr.jsonc -s ~/.config/waybar/style.css &> /dev/null & disown
--- .config/waybar/waybar-niri.sh
+++ .config/waybar/waybar-niri.sh
@@ -7,4 +7,4 @@ killall -q waybar
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
 # Launch main
-waybar -c ~/.config/waybar/config-niri.jsonc -s ~/.config/waybar/style-niri.css &
+waybar -c ~/.config/waybar/config-niri.jsonc -s ~/.config/waybar/style.css &
