8975f6361354cd8e02d8b45964934074b943dab5
Soma Noir <45913147+somanoir@users.noreply.github.com>
2025-11-08 10:27:09 +0300

    nvim: enable cursorline number highlights

--- .config/nvim/lua/plugins/cursorline-highlight.lua
+++ .config/nvim/lua/plugins/cursorline-highlight.lua
@@ -2,17 +2,17 @@ return {
   "ya2s/nvim-cursorline",
   enabled = true,
   config = function()
-    require('nvim-cursorline').setup {
+    require("nvim-cursorline").setup({
       cursorline = {
         enable = true,
         timeout = 1000,
-        number = false,
+        number = true,
       },
       cursorword = {
         enable = true,
         min_length = 3,
         hl = { underline = true },
-      }
-    }
+      },
+    })
   end,
 }
