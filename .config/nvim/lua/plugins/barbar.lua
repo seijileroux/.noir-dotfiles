df92b82b606b34be57d86b9ee0ec42561614c312
Soma Noir <45913147+somanoir@users.noreply.github.com>
2025-11-19 23:38:41 +0300

    nvim: remove redundant lazy init call

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,25 +1,23 @@
 return {
   "romgrk/barbar.nvim",
+  dependencies = {
+    "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+    "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+  },
   enabled = true,
   config = function()
-    require("lazy").setup({
-      {
-        "romgrk/barbar.nvim",
-        dependencies = {
-          "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-          "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-        },
-        init = function()
-          vim.g.barbar_auto_setup = false
-        end,
-        opts = {
-          -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-          -- animation = true,
-          -- insert_at_start = true,
-          -- …etc.
-        },
-        version = "^1.0.0", -- optional: only update when a new 1.x version is released
+    require("barbar").setup({
+      "romgrk/barbar.nvim",
+      init = function()
+        vim.g.barbar_auto_setup = false
+      end,
+      opts = {
+        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+        -- animation = true,
+        -- insert_at_start = true,
+        -- …etc.
       },
+      version = "^1.0.0", -- optional: only update when a new 1.x version is released
     })
     local map = vim.api.nvim_set_keymap
     local opts = { noremap = true, silent = true }
