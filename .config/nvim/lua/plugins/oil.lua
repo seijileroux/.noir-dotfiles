5bc99d35aec4359840869c4fb6c218098f9b0747
Seiji Leroux <45913147+seijileroux@users.noreply.github.com>
2025-12-03 10:22:23 +0300

    nvim: update oil keybinds

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -24,6 +24,22 @@ return {
         -- Set to "unmodified" to only save unmodified buffers
         autosave_changes = false,
       },
+      use_default_keymaps = false,
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
       view_options = {
         -- Show files and directories that start with "."
         show_hidden = true,
